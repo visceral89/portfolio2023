34cd710cacc8d7352b9343c7ccabcfc99ccd2333
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-01-26 21:48:59 +0100

    cleanuo

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import Head from "next/head";
-import Image from "next/image";
-import { motion } from "framer-motion";
 import { JetBrains_Mono } from "@next/font/google";
-import background from "assets/dotgrid.svg";
 
 const jetbrain = JetBrains_Mono({
 	subsets: ["latin"],
@@ -23,25 +20,8 @@ const Layout = ({ children }) => {
 			<Head>
 				<title>Rasmus Bremholm Portfolio</title>
 			</Head>
-			<motion.main
-				initial={{ x: 300, opacity: 0 }}
-				animate={{ x: 0, opacity: 1 }}
-				exit={{ x: 300, opacity: 0 }}
-				transition={{
-					type: "spring",
-					stiffness: 260,
-					damping: 20,
-				}}
-			>
-				<Image
-					className="bg-image"
-					src={background}
-					alt=""
-					layout="fill"
-					objectPosition="center"
-				/>
-				{children}
-			</motion.main>
+
+			{children}
 		</>
 	);
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,6 @@ import Image from "next/image";
 export default function Home() {
 	return (
 		<>
-			<Head>
-				<title>Rasmus Bremholm Portfolio</title>
-				<meta
-					name="description"
-					content="Rasmus Bremholm Frontend Developer Teacher 3D Artist"
-				/>
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-
 			<div>
 				<h1 className="text-8xl">rasmus bremholm</h1>
 				<h2>frontend portfolio</h2>
