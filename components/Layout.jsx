aa0b5f32e509746f665cb1af9532938ee48fbcc3
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-01-27 09:51:01 +0100

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -19,6 +19,11 @@ const Layout = ({ children }) => {
 					content="Rasmus Bremholm | Frontend Developer | 3D Artist | Teacher | SEO Expert"
 					key="title"
 				/>
+				<meta charset="UTF-8"></meta>
+				<meta
+					name="keywords"
+					content="Frontend, 3D, Teacher, Rasmus, Bremholm, Portfolio"
+				></meta>
 			</Head>
 
 			{children}
