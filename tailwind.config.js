89adffc7a1c97f7cb9c6eee0bbf3c7c1eae7c0c6
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-01-30 22:44:37 +0100

    Media Querie

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,6 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 module.exports = {
 	mode: "jit",
 	important: true,
-	purge: {
-		content: [
-			"./components/**/*.{js,ts,jsx,tsx}",
-			"./pages/**/*.{js,ts,jsx,tsx}",
-		],
-		options: {
-			safelist: {
-				standard: ["outline-none"],
-			},
-		},
-	},
 	content: [
 		"./app/**/*.{js,ts,jsx,tsx}",
 		"./pages/**/*.{js,ts,jsx,tsx}",
