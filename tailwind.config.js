6da9312fe659f186704d880b69011c6474fc7be2
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-01-30 21:05:18 +0100

    Purge CSS
    WARNING DELETE IF U GET PROBLEM

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	mode: "jit",
+	important: true,
+	purge: {
+		content: [
+			"./components/**/*.{js,ts,jsx,tsx}",
+			"./pages/**/*.{js,ts,jsx,tsx}",
+		],
+		options: {
+			safelist: {
+				standard: ["outline-none"],
+			},
+		},
+	},
 	content: [
 		"./app/**/*.{js,ts,jsx,tsx}",
 		"./pages/**/*.{js,ts,jsx,tsx}",
