bb926419962ad6752395b2b5dccc6c8af7ba2961
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-07-19 13:32:37 +0200

    Adding Navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Navbar from "./components/Navbar";
+
 
 export default function Home() {
 	return <main className={styles.main}></main>;
