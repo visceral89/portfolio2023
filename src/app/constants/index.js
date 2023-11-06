619283c5bbc32edfad4efdbcd947917a7a6aab78
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-11-06 20:37:29 +0100

    Create index.js

---
+++ src/app/constants/index.js
@@ -0,0 +1,33 @@
+export const titleText = [
+	{
+		id: "titleRole",
+		text: "Frontend Developer",
+	},
+];
+export const techStack = [
+	{
+		id: "next",
+		text: "Next JS 14",
+		imgUrl: "/nextjs.png",
+	},
+	{
+		id: "angular",
+		text: "Angular",
+		imgUrl: "/angular.png",
+	},
+	{
+		id: "three",
+		text: "Three JS",
+		imgUrl: "/three.png",
+	},
+	{
+		id: "blender",
+		text: "Blender",
+		imgUrl: "/blender.png",
+	},
+	{
+		id: "fusion",
+		text: "Fusion 360",
+		imgUrl: "/fusion.png",
+	},
+];
