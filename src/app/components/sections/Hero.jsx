cf45694980521b8cb81ff58986add699c86f8037
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-11-09 16:46:32 +0100

    added text to Hero

--- src/app/components/sections/Hero.jsx
+++ src/app/components/sections/Hero.jsx
@@ -1,5 +1,6 @@
 import styles from "../../styles/Hero.module.scss";
 import Image from "next/image";
+import Link from "next/link";
 
 const Hero = () => {
 	return (
@@ -7,14 +8,23 @@ const Hero = () => {
 			<div className={styles.titleText}>
 				<div className={styles.accent}>Frontend Developer</div>
 				<h1>Rasmus Bremholm Portfolio</h1>
+				<p>
+					Hi, welcome to my portfolio website! Im a frontend developer from
+					Gothenburg Sweden. Some people call me The Jack of All trades, and
+					master of some. I love to create, and here you will get a taste of
+					what I love to make!
+				</p>
+				<Link href='/contact'>
+					<div className={styles.contactButton}>Contact Me</div>
+				</Link>
 			</div>
 			<div className={styles.titleImage}>
-				<Image
+				{/* <Image
 					src='/guy_half.png'
 					width={800}
 					height={800}
 					alt='rasmus bremholm'
-				/>
+				/>*/}
 			</div>
 		</section>
 	);
