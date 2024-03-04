9084e6a3e6afa499ff4cf4356b9b1554f70797fc
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-03-04 20:14:36 +0100

    Making a Query

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,5 +1,12 @@
+async function getPosts() {
+	const query = `*[_type == "blog"] | order(_createdAt desc){
+		title,
+		smallDescription,
+		titleImage,
+		"currentSlug":slug.current
+	  }`;
+}
+
 export default function Page() {
 	return <h1>Blog Page Works</h1>;
 }
-
-
