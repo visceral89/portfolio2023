de4581461cd45b65ffb78e14d4293a17c528f609
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-04-03 22:17:06 +0200

    Cleanup

--- src/app/3d/page.jsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-	return <h1>Hello, 3D Page!</h1>;
-}
--- src/app/webdev/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-	return <div>Webdev Loaded</div>;
-};
-
-export default page;
