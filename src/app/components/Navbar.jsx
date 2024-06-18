79594e75fa4275800514c8025c5f36fe9fec82c3
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-06-18 09:22:18 +0200

    Removed hamburger

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,19 +1,7 @@
-"use client";
-
 import Link from "next/link";
 import styles from "../styles/Navbar.module.scss";
-import { useState } from "react";
-import MenuIcon from "@mui/icons-material/Menu";
-import useMediaQuery from "@mui/material/useMediaQuery";
 
 const Navbar = () => {
-	const [isOpen, setIsOpen] = useState(false);
-	const mobile = useMediaQuery("(max-width: 576px)");
-
-	const toggleMenu = () => {
-		setIsOpen(!isOpen);
-	};
-
 	return (
 		<nav className={styles.navBar}>
 			<Link href='/'>
@@ -23,17 +11,12 @@ const Navbar = () => {
 					</h1>
 				</div>
 			</Link>
-			{mobile ? (
-				<div className={styles.hamburgerMenu} onClick={toggleMenu}>
-					<MenuIcon />
-				</div>
-			) : (
-				<div className={styles.navBarPages}>
-					<Link href='/webdev'>frontend</Link>
-					<Link href='/blog'>blog</Link>
-					<Link href='/3d'>3D</Link>
-				</div>
-			)}
+
+			<div className={styles.navBarPages}>
+				<Link href='/webdev'>frontend</Link>
+				<Link href='/blog'>blog</Link>
+				<Link href='/3d'>3D</Link>
+			</div>
 		</nav>
 	);
 };
