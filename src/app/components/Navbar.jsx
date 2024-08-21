737056655590cf021e04d420ef852555a56ef765
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-08-21 15:33:39 +0200

    Navbar Work

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import styles from "./Navbar.module.scss";
+import Link from "next/link";
+
+export const Navbar = () => {
+	return (
+		<nav className={styles.navbar}>
+			<div className={styles.logo}>
+				<Link href='/'>Rasmus Bremholm</Link>
+			</div>
+
+			<div className={styles.links}>
+				<Link href='/blog'>Blog</Link>
+				<Link href='/contact'>Contact</Link>
+				<Link href='/about'>About</Link>
+			</div>
+		</nav>
+	);
+};
