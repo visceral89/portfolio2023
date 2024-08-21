c0379a50f51a079309e080940ebe2a7dc36f7e47
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-08-21 16:02:25 +0200

    tweaking css

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,16 @@
 import styles from "./page.module.scss";
 
 export default function Home() {
-	return <main className={styles.main}></main>;
+	return (
+		<main className={styles.main}>
+			<div className={styles.title}>
+				<h1>Rasmus Bremholm</h1>
+				<h2>Frontend Developer</h2>
+				<div className={styles.ctaContainer}>
+					<div className={styles.cta}>contact</div>
+					<div className={styles.cta}>github</div>
+				</div>
+			</div>
+		</main>
+	);
 }
