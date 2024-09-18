443ba98c69a11bccd6ebc27230c11d7cfb770b7c
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-09-18 09:56:21 +0200

    ok pause now for real

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -4,20 +4,22 @@ export const About = () => {
 	return (
 		<>
 			<h2>About Me</h2>
-			<h3>A bit about me</h3>
-			<p>
-				I'm a self-taught Frontend/Fullstack developer that is currently studying Frontend
-				Development at IT Högskolan in Göteborg, Sweden. I love exploring areas where tech and
-				artistry meet. With my previous work in E-commerce and education I have a keen sense for SEO
-				and best practises on the web to create blazing fast designs that also capture the human
-				element.
-			</p>
-			<p>
-				In my freetime I experiment with new technologies and use the 3D models I create to make
-				games. Please feel free to check out my Github to get a sense of what im working on
-				currently.
-			</p>
-			<h3>Technologies I love</h3>
+			<div className={styles.aboutContent}>
+				<h3>A bit about me</h3>
+				<p>
+					I'm a self-taught Frontend/Fullstack developer that is currently studying Frontend
+					Development at IT Högskolan in Göteborg, Sweden. I love exploring areas where tech and
+					artistry meet. With my previous work in E-commerce and education I have a keen sense for
+					SEO and best practises on the web to create blazing fast designs that also capture the
+					human element.
+				</p>
+				<p>
+					In my freetime I experiment with new technologies and use the 3D models I create to make
+					games. Please feel free to check out my Github to get a sense of what im working on
+					currently.
+				</p>
+				<h3>Technologies I love</h3>
+			</div>
 		</>
 	);
 };
