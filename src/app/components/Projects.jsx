b6d87b472e9e8047717b0f4850069c2475eea481
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-09-18 13:44:17 +0200

    grid fix

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -4,37 +4,40 @@ import Link from "next/link";
 
 export const Projects = () => {
 	return (
-		<div className={styles.wrapper}>
-			<article className={styles.projectCard}>
-				<Image src='/placeholder_650w.png' width={650} height={650} alt='project image 1' />
-				<div className={styles.projectCardContent}>
-					<h4>Project 1</h4>
-					<p>
-						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, ipsum vitae? Aut
-						temporibus accusantium vel!
-					</p>
-				</div>
-			</article>
-			<article className={styles.projectCard}>
-				<Image src='/placeholder_650w.png' width={650} height={650} alt='project image 1' />
-				<div className={styles.projectCardContent}>
-					<h4>Project 2</h4>
-					<p>
-						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, ipsum vitae? Aut
-						temporibus accusantium vel!
-					</p>
-				</div>
-			</article>
-			<article className={styles.projectCard}>
-				<Image src='/placeholder_650w.png' width={650} height={650} alt='project image 1' />
-				<div className={styles.projectCardContent}>
-					<h4>Project 3</h4>
-					<p>
-						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, ipsum vitae? Aut
-						temporibus accusantium vel!
-					</p>
-				</div>
-			</article>
-		</div>
+		<>
+			<h2>Recent Projects</h2>
+			<div className={styles.wrapper}>
+				<article className={styles.projectCard}>
+					<Image src='/placeholder_650w.png' width={650} height={650} alt='project image 1' />
+					<div className={styles.projectCardContent}>
+						<h4>Project 1</h4>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, ipsum vitae? Aut
+							temporibus accusantium vel!
+						</p>
+					</div>
+				</article>
+				<article className={styles.projectCard}>
+					<Image src='/placeholder_650w.png' width={650} height={650} alt='project image 1' />
+					<div className={styles.projectCardContent}>
+						<h4>Project 2</h4>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, ipsum vitae? Aut
+							temporibus accusantium vel!
+						</p>
+					</div>
+				</article>
+				<article className={styles.projectCard}>
+					<Image src='/placeholder_650w.png' width={650} height={650} alt='project image 1' />
+					<div className={styles.projectCardContent}>
+						<h4>Project 3</h4>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, ipsum vitae? Aut
+							temporibus accusantium vel!
+						</p>
+					</div>
+				</article>
+			</div>
+		</>
 	);
 };
