bc369ea9b6b4b1d746a7cc03b6bca880df25f924
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-09-22 11:58:12 +0200

    Adding Footer and Contact page

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,17 @@
+import styles from "./Footer.module.scss";
+
+export const Footer = () => {
+	return (
+		<footer>
+			<div>
+				<h4>Contact Information</h4>
+			</div>
+			<div>
+				<h4>Social Media</h4>
+			</div>
+			<div>
+				<h4>Navigation</h4>
+			</div>
+		</footer>
+	);
+};
