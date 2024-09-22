8276806277eccb45d8031252d9e5b032d7874c5c
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-09-22 19:11:22 +0200

    Contact classes
    plus an adjust to noise

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,10 +3,12 @@ import styles from "../page.module.scss";
 export default function Contact() {
 	return (
 		<main className={styles.main}>
-			<div className={styles.contactCard}>
+			<header className={styles.title}>
 				<h1>Contact Me</h1>
-                <h3>Let's get in touch!</h3>
-			</div>
+				<div className={styles.contactCard}>
+					<h3>Let's get in touch!</h3>
+				</div>
+			</header>
 		</main>
 	);
 }
