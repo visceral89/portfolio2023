6553b1215853f2971ebca31e653e8579b5c2b15f
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-09-22 14:00:20 +0200

    Contact Page Start

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,3 +1,12 @@
+import styles from "../page.module.scss";
+
 export default function Contact() {
-	return <main></main>;
+	return (
+		<main className={styles.main}>
+			<div className={styles.contactCard}>
+				<h1>Contact Me</h1>
+                <h3>Let's get in touch!</h3>
+			</div>
+		</main>
+	);
 }
