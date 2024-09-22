758296cfaeb211a1c1a29f04fb0a87c6a9dfa53a
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-09-22 13:56:52 +0200

    Footer done

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.scss";
 import "./layout.scss";
 import { Navbar } from "./components/Navbar.jsx";
+import { Footer } from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
 			<body className={inter.className}>
 				<Navbar />
 				{children}
+				<Footer />
 			</body>
 		</html>
 	);
