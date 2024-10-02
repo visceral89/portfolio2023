12560440fc387270b0c62a611a96391e110ecc71
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-10-02 08:13:06 +0200

    added typescript and sitemap generator

---
+++ src/app/sitemap.ts
@@ -0,0 +1,18 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+	return [
+		{
+			url: "https://rasmusbremholm.com",
+			lastModified: new Date(),
+			changeFrequency: "monthly",
+			priority: 1,
+		},
+		{
+			url: "https://rasmusbremholm.com/contact",
+			lastModified: new Date(),
+			changeFrequency: "monthly",
+			priority: 0.8,
+		},
+	];
+}
