2e30d2942d58f39c24879722d623e9bdaf96858a
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-10-02 13:31:49 +0200

    moore aria labels

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -11,9 +11,15 @@ export const Navbar = () => {
 				</Link>
 			</div>
 			<div className={styles.links}>
-				<Link href='/#projects'>Projects</Link>
-				<Link href='/contact'>Contact</Link>
-				<Link href='/#about'>About</Link>
+				<Link aria-label='projects' href='/#projects'>
+					Projects
+				</Link>
+				<Link aria-label='contact me' href='/contact'>
+					Contact
+				</Link>
+				<Link aria-label='about rasmus' href='/#about'>
+					About
+				</Link>
 			</div>
 		</nav>
 	);
