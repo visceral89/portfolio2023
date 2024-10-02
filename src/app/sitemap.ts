8dec8f4b4df394a3c5152ab15123720f5e91c7bb
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-10-02 13:58:25 +0200

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,6 +9,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
 			priority: 1,
 		},
 		{
+			url: "https://www.rasmusbremholm.com/projects",
+			lastModified: new Date(),
+			changeFrequency: "monthly",
+			priority: 0.9,
+		},
+		{
 			url: "https://www.rasmusbremholm.com/contact",
 			lastModified: new Date(),
 			changeFrequency: "monthly",
