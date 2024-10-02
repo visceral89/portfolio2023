94858b5b28e98ec80390fafd8bc87ee2cc2f73e7
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-10-02 12:52:23 +0200

    metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,8 @@ import "./globals.scss";
 import "./layout.scss";
 import { Navbar } from "./components/Navbar.jsx";
 import { Footer } from "./components/Footer";
+import { type } from "os";
+import { title } from "process";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,11 +12,35 @@ export const metadata = {
 	title: "Rasmus Bremholm | Portfolio",
 	description: "Rasmus Bremholm | Frontend Developer from Gothenburg Sweden",
 	openGraph: {
-		title: "Rasmus Bremholm | Portfolio",
+		title: "Rasmus Bremholm | Frontend Portfolio",
 		description:
 			"Rasmus Bremholm Portfolio, a frontend developer from Gothenburg Sweden looking for new oppurtunities",
 		url: "https://www.rasmusbremholm.com",
 		siteName: "Rasmus Bremholm Portfolio",
+		images: [
+			{
+				url: "https://www.rasmusbremholm.com/og-image.jpg",
+				width: 1200,
+				height: 630,
+				alt: "Rasmus Bremholm Portfolio Banner",
+			},
+		],
+		locale: "en_US",
+		type: "website",
+	},
+	twitter: {
+		card: "summary_large_image",
+		title: "Rasmus Bremholm | Frontend Developer",
+		description:
+			"Rasmus Bremholm Portfolio, a frontend developer from Gothenburg Sweden looking for new oppurtunities",
+		images: [
+			{
+				url: "https://www.rasmusbremholm.com/og-image.jpg",
+				width: 1200,
+				height: 630,
+				alt: "Rasmus Bremholm Portfolio Banner",
+			},
+		],
 	},
 };
 
